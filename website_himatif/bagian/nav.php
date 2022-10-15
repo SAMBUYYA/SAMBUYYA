3ead5e0214de81871db3f1abebebd2e416dcd33c
SAMBUYYA <83904969+SAMBUYYA@users.noreply.github.com>
2022-10-15 19:52:30 +0700

    Add files via upload

---
+++ website_himatif/bagian/nav.php
@@ -0,0 +1,15 @@
+<nav class="nav">
+
+<div>
+<a href="/"><img class="logo_lainnya" src="logo/email.svg"></a>
+<a href="https://www.youtube.com/channel/UCvRaOSJvdCKu1ZIqW2vuChA"><img class="logo_lainnya" src="logo/youtube.svg"></a>
+<a href="https://www.instagram.com/himatif.untara"><img class="logo_lainnya" src="logo/instagram.svg"></a>
+</div>
+
+<div class="halaman">
+<a href="/"><img src="logo/home.svg" width="18" height="18">Home</a>
+<a href="/tentang"><img src="logo/light.svg" width="18" height="18">About Us</a>
+<a href="/kontak"><img src="logo/paper-plane.svg" width="18" height="18">Contact Us</a>
+</div>
+
+</nav>
