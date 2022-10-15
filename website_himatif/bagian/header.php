c18ab95b7e67fc51b34f125ed3ff580d429b7fb5
SAMBUYYA <83904969+SAMBUYYA@users.noreply.github.com>
2022-10-15 20:00:32 +0700

    Delete website_himatif directory

--- website_himatif/bagian/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-
-
-<div class="logo"><img class="gambar" src="logo_himatif.png"></div>
-<div class="nama">
-    <p class="atas">Himpunan Mahasiswa Teknologi Informasi</p>
-    <p class="bawah"><span style="color:#C65D1B;">Universitas</span> <span style="color:#36098F;">Tangerang Raya</span></p>
-</div>
-</header>
